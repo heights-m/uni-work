0e4fe69142933fe70ebf76d6a2e3f0b674a0969b
heights <68572517+heights-m@users.noreply.github.com>
2024-04-02 17:48:26 +0900

    Update command.ml

--- CSE216/homework/robot/command.ml
+++ CSE216/homework/robot/command.ml
@@ -21,7 +21,12 @@ let moveto_pose b_camera (pose, board) target_pose =
       let rec draw_frames step_cnt chpose =
         if step_cnt = 0
         then chpose
-        else 
+        else begin
+          let new_pose = chg_pose chpose joint ang_seg in
+          draw_frame new_pose;
+          draw_frames (step_cnt-1) new_pose
+        end in
+      draw_frames step pose in
         
         (*TODO: implement this method
             - on each step, draw the robot and the board
@@ -42,6 +47,9 @@ let moveto_pose b_camera (pose, board) target_pose =
 let pick (pose, board) i =
     let f = get_pose pose "finger" in
     let m = get_mark board i in
+    let b = chg_mark board i mark_n in
+    let p = (get_pose pose "base", get_pose pose "arm1", get_pose pose "arm2", 0., m) in
+    (p, b)
     (*TODO: return (p, b), where p is the pose whose
             mark and finger are switched to m and 0. respectively
             and b is the board whose mark at i is set to mark_n.
@@ -54,6 +62,9 @@ let drop (pose, board) i =
     let f = get_pose pose "finger" in
     let m = get_pose pose "mark" in
     let j = if m = mark_o then 9 else 10 in
+    let p = (get_pose pose "base", get_pose pose "arm1", get_pose pose "arm2", (10. -. f), mark_n) in
+    let b = chg_mark board i m |> fun b -> chg_mark b j m in
+    (p, b)
     (*TODO: return (p, b),
             where b is the board whose i-th and j-th marks are m
             and p is the pose whose mark and finger are set 
@@ -73,11 +84,18 @@ let mark b_camera (pose, board) mrk dst =
                using find_pose, mark_pos then
             2) pass two params for the fun returned by find_pose
     *)            
-    let dst_pose = in (*robot's pose for the dst-th mark with finger is f, mark is mrk*)
-    let src_pose = in (*robot's pose for the src-th mark with finger is 0, mark is m*)
+    let dst_pose = find_pose (mark_pos dst) f mrk in (*robot's pose for the dst-th mark with finger is f, mark is mrk*)
+    let src_pose = find_pose (mark_pos src) 0. m in (*robot's pose for the src-th mark with finger is 0, mark is m*)
 
     (*moveto_pose with the first param applied*)
     let mvp = moveto_pose b_camera in
+
+    mvp (pose, board) src_pose |> fun b ->
+    pick b src |> fun b ->
+    mvp b (lift_pose b) |> fun b ->
+    mvp b dst_post |> fun b ->
+    drop b dst |> fun b ->
+    mvp b (lift_pose b) 
     
     (*TODO: 1. move to pose src_pose (use mvp)
             2. pick the mark at src  (use pick)
