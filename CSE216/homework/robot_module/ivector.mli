01911c301153e8e87a6e7275bb8c57991c7327d5
heights <68572517+heights-m@users.noreply.github.com>
2024-04-17 17:47:24 +0900

    Update ivector.mli

--- CSE216/homework/robot_module/ivector.mli
+++ CSE216/homework/robot_module/ivector.mli
@@ -3,6 +3,17 @@
 -------------------------------------*)
 open Globals
 
+module type Ivect = sig
+
+val add: vector -> vector -> vector
+val sub: vector -> vector -> vector
+val smul: float -> vector -> vector
+val prod: vector -> vector -> float
+val len: vector -> float
+val rotx: float -> vector -> vector
+val roty: float -> vector -> vector
+val rotz: float -> vector -> vector
+end
 (*TODO: Add a module signature IVect.
   It should expose functions: add, sub, smul, prod, len, rotx, roty, rotz
   hint: use vector type of globals.ml
