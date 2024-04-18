636ac115165a1a84a6ad7185981afd6eced079c8
heights <68572517+heights-m@users.noreply.github.com>
2024-04-18 18:19:05 +0900

    Update pose.ml

--- CSE216/homework/robot_module/pose.ml
+++ CSE216/homework/robot_module/pose.ml
@@ -12,7 +12,7 @@ open Ipose
         Vect of IVect type, Basis of IBasis type, Board of IBoard type in this order
     and implements the signature IPose*)
 
-
+module PoseImpl (Vect: IVect) (Basis: IBasis) (Board: IBoard): IPose = struct
     (*the position of the mark at index i*)   
     let mark_pos = function
         | 0 -> ( 0.2, 0.8, 0.)
@@ -30,7 +30,13 @@ open Ipose
 
     (*the specified angle of the pose*)
     let get_pose (b, a1, a2, f, m) =
-        function
+        function x ->
+            if x = "base" then b
+	         else if x = "arm1" then a1
+	         else if x = "arm2" then a2
+	         else if x = "finger" then f
+	         else if x = "mark" then m
+	         else assert false
         (*TODO: return b, a1, a2, f, and m for the parameters
                 "base", "arm1", "arm2", "finger", and "mark"
             e.g. get_pose (0., 1., 2., 3., 1.) "arm2"
@@ -39,6 +45,12 @@ open Ipose
 
     (*the pose whose joint is changed by delta*)
     let chg_pose (b, a1, a2, f, m) joint delta = 
+          let checker tag ang =
+               if tag = joint then 
+		            if tag = "mark" then delta
+		            else ang +. delta
+	            else ang in
+          (checker "base" b, checker "arm1" a1, checker "arm2" a2, checker "finger" f, checker "mark" m)
         (*TODO: return the pose whose angles are switched to
                 b+delta, a1+delta, a2+delta, f+delta, or delta
                 depending on the joint parameters of
@@ -52,6 +64,12 @@ open Ipose
         fun f m ->
             let d1 = 0.5 in  (*length of arm1*)
             let d2 = 0.55 in (*length of arm2 + length of finger*)
+            let b = rad2deg (atan2 y x) in
+         	let d = sqrt (x *. x +. y *. y +. z *. z) in
+         	let a2 = acos ( (d *. d -. d1 *. d1 -. d2 *. d2) /. (2. *. d1 *. d2)) in
+         	let a2d = rad2deg a2 in
+         	let a1 = 90. -. rad2deg (asin (z /. d) +. asin (d2 *. (sin a2) /. d)) in
+         	(b, a1, a2d, f, m)
             (*TODO: find b, a1, and a2 and return the pose (b, a1, a2, f, m)
                     b: angle (deg) of base measured from x axis (use atan2),
                     a1: angle (deg) of arm1 measured from z axis
